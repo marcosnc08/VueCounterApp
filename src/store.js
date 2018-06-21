18f37ab9b5d7c6d85d1561d53aab90cbb780ef45
Marcos <marcosnc.08@gmail.com>
2018-06-21 12:53:28 -0300

    counter app initial commit

--- src/store.js
+++ src/store.js
@@ -5,12 +5,31 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    countValue: 0
   },
   mutations: {
-
+    increment(state, payload) {
+        state.countValue = state.countValue + payload.valueToAdd;
+    },
+    decrement(state, payload) {
+        state.countValue = state.countValue + payload.valueToAdd;
+    }
   },
   actions: {
-
+    increment(context) {
+        setTimeout(function() {
+            context.commit('increment', { valueToAdd: 1 });
+        }, 500);
+    },
+    decrement(context) {
+        setTimeout(function() {
+            context.commit('decrement', { valueToAdd: -1 });
+        }, 500);
+    },
+    incrementByValue(context, payload) {
+        setTimeout(function() {
+            context.commit('increment', payload);
+        }, 500);
+    },
   }
 })
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,42 @@
 <template>
   <div class="home">
     <img src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Welcome to counter App</h1>
+    <p>{{ this.countValue }}</p>
+    <p>
+        <a @click="this.add" href="#">Add</a> | <a @click="this.substract" href="#">Substract</a>
+    </p>
+    <p>
+        <input type="text" ref="inputValueToAdd"> <a @click="this.addValue" href="#">Add Value</a>
+    </p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'home',
-  components: {
-    HelloWorld
-  }
+  computed: {
+    countValue: function() {
+        return this.$store.state.countValue;
+    }
+  },
+  methods: {
+    add: function() {
+        this.$store.dispatch('increment');
+    },
+    substract: function() {
+        this.$store.dispatch('decrement');
+    },
+    addValue: function() {
+        this.$store.dispatch('incrementByValue', { valueToAdd: this.getValueToAdd() });
+    },
+    getValueToAdd: function() {
+        var inputText = this.$refs.inputValueToAdd.value.trim();
+        if(inputText == '') 
+            return 0;
+        
+        return parseInt(inputText, 10);
+    },
+  },
 }
 </script>
